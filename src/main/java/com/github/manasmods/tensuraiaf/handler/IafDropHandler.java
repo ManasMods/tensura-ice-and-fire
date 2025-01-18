adb5b74734eff5b7a9769ac25a67ad88a9af5e8c
Minh <minheragon12345@gmail.com>
2025-01-18 22:16:20 +0700

    Dragon Corpse dropping EP and Essences

--- src/main/java/com/github/manasmods/tensuraiaf/handler/IafDropHandler.java
+++ src/main/java/com/github/manasmods/tensuraiaf/handler/IafDropHandler.java
@@ -1,6 +1,9 @@
 package com.github.manasmods.tensuraiaf.handler;
 
 import com.github.alexthe666.iceandfire.entity.EntityDragonBase;
+import com.github.manasmods.tensura.event.EnergyDrainEvent;
+import com.github.manasmods.tensura.event.SkillPlunderEvent;
+import com.github.manasmods.tensura.event.SpiritualHurtEvent;
 import com.github.manasmods.tensura.registry.items.TensuraMobDropItems;
 import com.github.manasmods.tensuraiaf.TensuraIaF;
 import com.github.manasmods.tensuraiaf.TensuraIafConfig;
@@ -22,5 +25,26 @@ public class IafDropHandler {
         if (dragon.getDragonStage() < TensuraIafConfig.INSTANCE.minDragonStageForEssence.get()) return;
         if (dragon.getDragonStage() > TensuraIafConfig.INSTANCE.maxDragonStageForEssence.get()) return;
         dragon.spawnAtLocation(TensuraMobDropItems.DRAGON_ESSENCE.get());
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onEnergyDrain(final EnergyDrainEvent e) {
+        if (e.isCanceled()) return;
+        if (!(e.getEntity() instanceof EntityDragonBase dragon)) return;
+        if (dragon.isModelDead() || dragon.getDeathStage() > 0) e.setCanceled(true);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onSkillPlunder(final SkillPlunderEvent e) {
+        if (e.isCanceled()) return;
+        if (!(e.getTarget() instanceof EntityDragonBase dragon)) return;
+        if (dragon.isModelDead() || dragon.getDeathStage() > 0) e.setCanceled(true);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onSpiritualHurt(final SpiritualHurtEvent e) {
+        if (e.isCanceled()) return;
+        if (!(e.getEntity() instanceof EntityDragonBase dragon)) return;
+        if (dragon.isModelDead() || dragon.getDeathStage() > 0) e.setCanceled(true);
     }
 }
