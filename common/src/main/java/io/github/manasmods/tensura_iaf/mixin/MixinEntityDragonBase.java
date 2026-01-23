187dc1975fe652005e42b70e2b94fb3417c4270f
Minh <minh1604.work@gmail.com>
2026-01-24 02:56:44 +0700

    Config fixes

--- common/src/main/java/io/github/manasmods/tensura_iaf/mixin/MixinEntityDragonBase.java
+++ common/src/main/java/io/github/manasmods/tensura_iaf/mixin/MixinEntityDragonBase.java
@@ -1,8 +1,9 @@
 package io.github.manasmods.tensura_iaf.mixin;
 
 import com.iafenvoy.iceandfire.entity.DragonBaseEntity;
+import io.github.manasmods.manascore.config.ConfigRegistry;
 import io.github.manasmods.tensura.registry.item.TensuraMobDropItems;
-import io.github.manasmods.tensura_iaf.TensuraIaf;
+import io.github.manasmods.tensura_iaf.registry.IafConfig;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(DragonBaseEntity.class)
 public abstract class MixinEntityDragonBase extends TamableAnimal {
+
     protected MixinEntityDragonBase(EntityType<? extends TamableAnimal> entityType, Level level) {
         super(entityType, level);
     }
@@ -19,9 +21,11 @@ public abstract class MixinEntityDragonBase extends TamableAnimal {
     @Override
     protected void dropCustomDeathLoot(ServerLevel serverLevel, DamageSource damageSource, boolean bl) {
         DragonBaseEntity dragon = (DragonBaseEntity) (Object) this;
-        if (dragon.getDragonStage() < TensuraIaf.CONFIG.minStageEssence) return;
-        if (dragon.getDragonStage() > TensuraIaf.CONFIG.maxStageEssence) return;
-        if (dragon.getRandom().nextFloat() > TensuraIaf.CONFIG.essenceChance) return;
+        IafConfig CONFIG = ConfigRegistry.getConfig(IafConfig.class);
+
+        if (dragon.getDragonStage() < CONFIG.minStageEssence) return;
+        if (dragon.getDragonStage() > CONFIG.maxStageEssence) return;
+        if (dragon.getRandom().nextFloat() > CONFIG.essenceChance) return;
         dragon.spawnAtLocation(TensuraMobDropItems.DRAGON_ESSENCE.get());
     }
 }
